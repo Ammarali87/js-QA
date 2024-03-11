c38c92cf4c674a5f7e7a79df1320267c69590f7c
Amar Ibrahim <113317548+Ammarali87@users.noreply.github.com>
2024-03-11 23:02:38 +0200

    two

---
+++ java.js
@@ -0,0 +1,17 @@
+
+
+ var quote = document.getElementById("quote")
+
+
+    var listquote = []
+
+	function newquote(){
+		
+		return  
+		var getnum = math.floor(math.random() * 10 ) + 1 
+		
+		for (var i = 0; i < 10; i++) {
+			listquote.push(newquote())
+		}
+     console.log(listquote)
+  }
